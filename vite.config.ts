1748537aa2fa2cd9513bf8002e50a92efab1e465
siwasu17 <siwasu17@gmail.com>
2026-02-10 00:22:13 +0900

    Fix trivial

--- vite.config.ts
+++ vite.config.ts
@@ -22,11 +22,13 @@ export default defineConfig({
             src: 'holireco_192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable',
           },
           {
             src: 'holireco_512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
